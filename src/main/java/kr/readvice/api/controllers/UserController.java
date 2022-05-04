da10e2e0b4319c08f7529e55ab1a48b0e1c867b8
meanseo <chlalstjok97@gmail.com>
2022-05-04 18:11:16 +0900

    controller-repository-service 연결

--- src/main/java/kr/readvice/api/controllers/UserController.java
+++ src/main/java/kr/readvice/api/controllers/UserController.java
@@ -1,10 +1,16 @@
 package kr.readvice.api.controllers;
 
+import kr.readvice.api.domains.User;
 import kr.readvice.api.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,13 +19,60 @@ public class UserController {
 
     private final UserService service;
 
-    @PostMapping("/join") public String register(){
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+
+        return service.login(user);
+    }
+    @GetMapping("/logout")
+    public String logout(){
         return "";
     }
-    @PostMapping("/login") public String login(){
-        return "";
+
+    @GetMapping("/findAll")
+    public List<User> findAll() {
+        return service.findAll();
     }
-    @PostMapping("/logout") public String logout(){
-        return "";
+    @GetMapping("/findAll/sort")
+    public List<User> findAll(Sort sort) {
+        return service.findAll(sort);
     }
+    @GetMapping("/findAll/pageable")
+    public Page<User> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
+    @GetMapping("/count")
+    public long count() {
+        return service.count();
+    }
+    @PutMapping("/put")
+    public String put (@RequestBody User user){
+        return service.put(user);
+    }
+    @DeleteMapping("/delete")
+    public String delete(@RequestBody User user) {
+        return service.delete(user);
+    }
+    @PostMapping("/join") public String save(@RequestBody User user) {
+        return service.save(user);
+    }
+    @GetMapping("/findById/{userid}")
+    public Optional<User> findById(@PathVariable String userid) {
+        return service.findById(userid);
+    }
+    @GetMapping("/existsById/{userid}")
+    public boolean existsById(@PathVariable String userid) {
+        return service.existsById(userid);
+    }
+
+    @PostMapping("/join")
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @PostMapping("/join")
+    public <S extends User> boolean exists(Example<S> example) {
+        return false;
+    }
+
 }
--- src/main/java/kr/readvice/api/repositories/UserRepository.java
+++ src/main/java/kr/readvice/api/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package kr.readvice.api.repositories;
 
-public interface UserRepository {
+import kr.readvice.api.domains.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    void put(User user);
+    String login(User user);
 }
